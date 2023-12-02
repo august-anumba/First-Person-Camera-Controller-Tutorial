3f97fd27e2eb473eb9cf915641ccfba8e0e6ab39
august.anumba <chinedu.anumba@outlook.com>
2023-12-02 18:46:28 +0000

    Updated Commit 1

--- Assets/Scenes/PlayerCam.cs
+++ Assets/Scenes/PlayerCam.cs
@@ -27,8 +27,8 @@ public class PlayerCam : MonoBehaviour
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
         yRotation += mouseX;
-
         xRotation -= mouseY;
+
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
