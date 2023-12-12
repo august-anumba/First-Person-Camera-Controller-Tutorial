3a991ddb94e448e942de326bdeacb499cf7ba0be
august-anumba <chinedu.anumba@outlook.com>
2023-12-12 17:46:34 +0000

    Commit

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -6,13 +6,11 @@ public class MoveCamera : MonoBehaviour
 {
     public Transform cameraPosition;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = cameraPosition.position;
